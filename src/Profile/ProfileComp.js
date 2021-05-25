12b719a4fdc7c7f4bb8a61eef748be559616d595
Rihab Chouikh <chouikh.rihab@gmail.com>
2021-05-25 14:44:07 +0200

    inline Style

--- src/Profile/ProfileComp.js
+++ src/Profile/ProfileComp.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import './profile.css';
 import propTypes from "prop-types";
-
+const S={display: "inline",
+    fontFamily:"Georgia, Times New Roman, Times, serif" ,
+    fontSize: "1.114rem",
+    fontWeight:"bold" ,
+    maxWidth: "19rem",
+    textAlign: "center",
+    margin: "1rem 0.789rem" ,
+    padding:"3rem 0"}
 const ProfileComp = (props) => {
     console.log("props=",props)
     return (
     
         <div >
             
-            <ul>
-            <li>{props.children}</li>
-            <li><span className="title">Full Name :</span> {props.fullName}</li>
-            <li><span className="title">Profession :</span> {props.profession}</li>
-            <li><span className="title">Bio :</span> {props.bio}</li>
-            <li><button onClick={()=>props.handleName(props.fullName)}>User Name</button></li>
+            <ul >
+            <li style={S}>{props.children}</li>
+            <li style={S}><span className="title">Full Name :</span> {props.fullName}</li>
+            <li style={S}><span className="title">Profession :</span> {props.profession}</li>
+            <li style={S}><span className="title">Bio :</span> {props.bio}</li>
+            <li style={S}><button onClick={()=>props.handleName(props.fullName)}>User Name</button></li>
             </ul>
         </div>
         
